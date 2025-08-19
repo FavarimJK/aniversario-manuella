9e2a4c720745a09b7b64d8242a299de021152fe9
Kleber José Favarim <kjf.pericia@gmail.com>
2025-08-18 21:32:01 -0300

    Primeiro upload do site 🎉
    Primeiro upload do site 🎉

---
+++ fotos.js
@@ -0,0 +1,26 @@
+const fotos = {
+  principal: ["manuella.jpg"],
+
+  amigos: [
+    "amigos1.jpg","amigos2.jpg","amigos3.jpg",
+    "amigos4.jpg","amigos5.jpg","amigos6.jpg",
+    "amigos7.jpg","amigos8.jpg","amigos9.jpg",
+    "amigos10.jpg","amigos11.jpg","amigos12.jpg","amigos13.jpg"
+  ],
+
+  familia: [
+    "familia1.jpg","familia2.jpg","familia3.jpg","familia4.jpg",
+    "familia5.jpg","familia6.jpg","familia7.jpg","familia8.jpg",
+    "familia9.jpg","familia10.jpg","familia11.jpg","familia12.jpg",
+    "familia13.jpg","familia14.jpg","familia15.jpg","familia16.jpg",
+    "familia17.jpg","familia18.jpg","familia19.jpg","familia20.jpg"
+  ],
+
+  momentos: [
+    "momentos1.jpg","momentos2.jpg","momentos3.jpg","momentos4.jpg",
+    "momentos5.jpg","momentos6.jpg","momentos7.jpg","momentos8.jpg",
+    "momentos9.jpg","momentos10.jpg","momentos11.jpg","momentos12.jpg",
+    "momentos13.jpg","momentos14.jpg","momentos15.jpg","momentos16.jpg",
+    "momentos17.jpg","momentos18.jpg"
+  ]
+};
